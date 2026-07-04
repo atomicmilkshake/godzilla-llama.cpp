0cdbeb68c25769e8796fbad2203c1b0e9f37c13f
atomicmilkshake <atomicmilkshake@users.noreply.github.com>
2026-07-04 10:49:51 -0500

    Update monitor script with newest run ID

--- scratch/monitor_release.py
+++ scratch/monitor_release.py
@@ -10,7 +10,7 @@ def main():
     if hasattr(sys.stdout, "reconfigure"):
         sys.stdout.reconfigure(encoding="utf-8")
         
-    run_id = "28711235159"
+    run_id = "28711432160"
     repo = "atomicmilkshake/godzilla-llama.cpp"
     console = Console()
     
